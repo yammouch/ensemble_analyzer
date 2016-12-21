9fc4e4bb605cb37b612a149bdcb578a31c224705
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-12-22 08:43:51 +0900

    Updated.

--- 0030_swing/src/ensemble_analyzer/chromatic.clj
+++ 0030_swing/src/ensemble_analyzer/chromatic.clj
@@ -1,57 +1,26 @@
 (ns ensemble-analyzer.chromatic)
 
-; hi : high note (A4 (440 or 442 Hz) = 0)
-; lo : low note
+; lo : low freq in chromatic plot
+; hi : high freq in chromatic plot
 ; np : number of pixels
-; sm : spectrum magnitude
-; sr : sampling rate
-; fa : frequency of A4 (440 or 442 Hz, typically)
-;(defn lin-freq-to-chromatic [hi lo np sm sr fa]
-;  (let [nf (count sm) ; number of FFT points
-;        fr (/ sr nf) ; freq resolution
-;        fl (* fa (Math/pow 2.0 (/ lo 12.0))) ; low freq in chromatic
-;        fp (Math/pow 2.0 (/ (- hi lo) (* 12.0 npix))) ; freq ratio in a pixel
-;        sw (Math/floor (/ (Math/log (/ fd (* (- fp 1.0) fl)))
-;                          (Math/log fp)))
-;        fp-rt (Math/sqrt fp)
-;        end (min np sw)
-;        [acc sm fc fs]
-;        (loop [acc (repeat np 0.0), sm sm, i 0, fc (* fl fp-rt), fs fr]
-;          (cond (or (<= sw end) (empty? sm)) [acc sm fc fs]
-;                (<= fc fs) (recur (assoc acc i (first sm)) sm (inc i)
-;                                  (* fc fp) fs)
-;                :else (recur acc (next sm) i fc (+ fs fr))))]
-;    (loop [acc acc, sm sm, i sw,
-;           fc (/ fc fp-rt), fs (- fs (* 0.5 fs)), sum 0.0, nsum 0]
-;      (cond (or (<= sw np) (empty? sm)) acc
-;            (<= fc fs) (recur (assoc acc i (/ sum nsum)) sm (inc i)
-;                              (* fc fp) fs 0.0 0)
-;            :else (recur acc (next sm) i fc (+ fs fr)
-;                         (+ sum (first sm)) (inc nsum)
-;                         )))))
-
-; lo : low freq of result
-; hi : high freq of result
-; np : number of pixels
-; sm : spectrum magnitude
 ; fr : freq resolution
-(defn lin-to-log [lo hi np sm fr]
-  (let [fp (Math/pow (/ hi lo) (/ 1.0 np))
-        sw (Math/floor (/ (Math/log (/ fr (* (- fp 1.0) lo)))
-                          (Math/log fp)))
-        fp-rt (Math/sqrt fp)
-        end (min np sw)
-        [acc sm fc fs]
-        (loop [acc (repeat np 0.0), sm sm, i 0, fc (* lo fp-rt), fs fr]
-          (cond (or (<= sw end) (empty? sm)) [acc sm fc fs]
-                (<= fc fs) (recur (assoc acc i (first sm)) sm (inc i)
-                                  (* fc fp) fs)
-                :else (recur acc (next sm) i fc (+ fs fr))))]
-    (loop [acc acc, sm sm, i sw,
-           fc (/ fc fp-rt), fs (- fs (* 0.5 fs)), sum 0.0, nsum 0]
-      (cond (or (<= sw np) (empty? sm)) acc
-            (<= fc fs) (recur (assoc acc i (/ sum nsum)) sm (inc i)
-                              (* fc fp) fs 0.0 0)
-            :else (recur acc (next sm) i fc (+ fs fr)
-                         (+ sum (first sm)) (inc nsum)
-                         )))))
+(defn index [lo hi np fr n-valid-fft-bins]
+  (let [[llog hlog] (map #(Math/log %) [lo hi])
+        f (map #(Math/exp (+ llog (/ (* (- hlog llog) %)
+                                     np)))
+               (range (inc np)))
+        fnorm (map #(/ % fr) f)]
+    (prn f)
+    (prn f fnorm)
+    (map (fn [[l h]]
+           (cond (<= (- n-valid-fft-bins 0.5) l) nil
+
+                 (< (- h l) 1.0)
+                 (let [m (int (+ 0.5 (* 0.5 (+ h l))))]
+                   (if (<= n-valid-fft-bins m) nil m))
+
+                 :else
+                 (let [u (int (+ 0.5 h))]
+                   [(int (+ 0.5 l))
+                    (if (<= n-valid-fft-bins u) n-valid-fft-bins u)])))
+         (partition 2 1 fnorm))))
--- 0030_swing/test/ensemble_analyzer/chromatic_test.clj
+++ 0030_swing/test/ensemble_analyzer/chromatic_test.clj
@@ -5,18 +5,5 @@
 
 (deftest a-test
   (testing "testcase 1"
-    (prn (lin-to-log 32.0 1.0 5
-                     [ 1.0 ;  0 Hz ( 0- 2 Hz)
-                       2.0 ;  4 Hz ( 2- 6 Hz)
-                       3.0 ;  8 Hz ( 6-10 Hz)
-                       4.0 ; 12 Hz (10-14 Hz)
-                       5.0 ; 16 Hz (14-18 Hz)
-                       6.0 ; 20 Hz (18-22 Hz)
-                       7.0 ; 24 Hz (22-26 Hz)
-                       8.0 ; 28 Hz (26-30 Hz)
-                       9.0 ; 32 Hz (30-34 Hz)
-                      10.0 ; 36 Hz (34-38 Hz)
-                      11.0 ; 40 Hz (38-42 Hz)
-                      ]
-                     4.0))
+    (prn (index 0.25 256.0 10 4.0 16))
     (is true)))
