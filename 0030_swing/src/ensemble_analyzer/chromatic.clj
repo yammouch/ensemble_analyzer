d59737fc3b0cd74937edfcf7001516e4571f90ad
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-12-22 11:34:25 +0900

    'pickup' has been added.

--- 0030_swing/src/ensemble_analyzer/chromatic.clj
+++ 0030_swing/src/ensemble_analyzer/chromatic.clj
@@ -10,17 +10,25 @@
                                      np)))
                (range (inc np)))
         fnorm (map #(/ % fr) f)]
-    (prn f)
-    (prn f fnorm)
     (map (fn [[l h]]
-           (cond (<= (- n-valid-fft-bins 0.5) l) nil
+           (cond (<= (- n-valid-fft-bins 0.5) l) []
 
                  (< (- h l) 1.0)
                  (let [m (int (+ 0.5 (* 0.5 (+ h l))))]
-                   (if (<= n-valid-fft-bins m) nil m))
+                   (if (<= n-valid-fft-bins m) [] [m]))
 
                  :else
                  (let [u (int (+ 0.5 h))]
-                   [(int (+ 0.5 l))
-                    (if (<= n-valid-fft-bins u) n-valid-fft-bins u)])))
+                   (range (int (+ 0.5 l))
+                          (if (<= n-valid-fft-bins u) n-valid-fft-bins u)
+                          ))))
          (partition 2 1 fnorm))))
+
+(defn pickup [vv sm] ; vv, vector of vercors, sm : spectrum magnitude
+  (map (fn [idxs]
+         (if (empty? idxs)
+           0.0
+           (/ (apply + (map (fn [idx] (get sm idx))
+                            idxs))
+              (count idxs))))
+       vv))
--- 0030_swing/test/ensemble_analyzer/core_test.clj
+++ 0030_swing/test/ensemble_analyzer/core_test.clj
@@ -3,5 +3,4 @@
             [ensemble-analyzer.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (is (= 1 1)))
