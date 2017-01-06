d9f402aac9ae70be16c0cad948691f35c3b3a8f1
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-01-06 19:05:28 +0900

    Image changes, size also changes.

--- 0070_image/src/image/core.clj
+++ 0070_image/src/image/core.clj
@@ -9,25 +9,53 @@
         '(java.awt.image BufferedImage)
         '(javax.swing JFrame JPanel ImageIcon JLabel))
 
+(require 'clojure.pprint)
+
 (defn make-panel []
   (proxy [JPanel] []
     (getPreferredSize [] 
-      (Dimension. 200 200)
+      (Dimension. 600 600)
       )))
 
+(def img (ref nil))
+(def panel (ref nil))
+
 (defn make-frame []
-  (let [frame (JFrame. "Ensemble Analyzer")
-        panel (make-panel)
-        img (BufferedImage. 200 200 BufferedImage/TYPE_INT_ARGB)]
-    (.setRGB img 0 0 200 200
-     (int-array (range 0xFF000000 (+ 0xFF000000 40000)))
-     0 200)
-    (.add panel (JLabel. (ImageIcon. img)))
-    (.. frame getContentPane (add panel BorderLayout/CENTER))
+  (let [frame (JFrame. "Ensemble Analyzer")]
+    (dosync
+     (ref-set panel (make-panel))
+     (ref-set img
+      (BufferedImage. 600 600 BufferedImage/TYPE_INT_ARGB)))
+    (.setRGB @img 0 0 600 600
+     (int-array (range 0xFF000000 (+ 0xFF000000 360000)))
+     0 600)
+    (.add @panel (JLabel. (ImageIcon. @img)))
+    (.. frame getContentPane (add @panel BorderLayout/CENTER))
     (.pack frame)
     (.setVisible frame true)
     frame))
 
 (defn -main [& args]
   (let [frame (make-frame)]
+    (Thread/sleep 2000)
+
+    (println "2 seconds.")
+    (.setRGB @img 0 0 600 600
+     (int-array (range 0xFFFF0000 (- 0xFFFF0000 360000) -1))
+     0 600)
+    (.repaint @panel)
+    (Thread/sleep 2000)
+
+    (println "2 seconds.")
+    (dosync 
+      (ref-set img
+       (BufferedImage. 200 200 BufferedImage/TYPE_INT_ARGB)))
+    (.setRGB @img 0 0 200 200
+     (int-array (range 0xFF000000 (+ 0xFF000000 40000)))
+     0 200)
+    (.removeAll @panel)
+    (.add @panel (JLabel. (ImageIcon. @img)))
+    (.validate @panel)
+    (.repaint @panel)
+
     :done))
